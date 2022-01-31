653acd075a90590bb2e99b984fadc15800ec9934
MiloMontuori <milo.montuori@student.junia.com>
2022-01-31 08:29:36 +0100

    up 1075

--- 1075/tas.c
+++ 1075/tas.c
@@ -31,27 +31,64 @@ int insert(int tab[], int pos){
     }
 }
 
-int tri_tas(int tab[], char type){
+int remove(int tab[], int pos){
+    int currentChild, leftChild, rightChild, maxChild;
+    bool stop;
+    currentChild = 0;
+    stop = false;
+    swap (tab[0],tab[pos]);
+    leftChild = 2*currentChild + 1;
+    rightChild = 2*currentChild + 2;
+    while ((!stop) && ((leftChild) <= (pos - 1)))
+    {
+        if ((leftChild = pos - 1) || (tab[leftChild] > tab[rightChild])){
+            maxChild = leftChild;
+        }
+        else{
+            maxChild = rightChild;
+        }
+        if (tab[currentChild < tab[maxChild]])
+        {
+            swap(tab[currentChild], tab[maxChild]);
+            currentChild = maxChild;
+            leftChild = 2*currentChild + 1;
+            rightChild = 2*currentChild + 2;
+        }
+        else{
+            stop = true;
+        }
+        
+    }
+    
+}
+
+int tri_tas(char type){
     int tab[taille];
     if (type == "a")
         // Création d'un tableau de valeur aléatoire
         for( int i = 0; i < taille; i++){
-            tab[i] = (int)((double)rand() / ((double)RAND_MAX + 1) * ((double)1000 - (double)0)) + 0;}
+            tab[i] = (int)((double)rand() / ((double)RAND_MAX + 1) * ((double)1000 - (double)0)) + 0;
+        }
     if (type == "c")
         // Création d'un tableau de valeur ordonné croissant
-        for (int i = 0; i < taille; i++){
-            tab[i] = i;}
+        for (int i = 0; i < taille; i++)
+        {
+            tab[i] = i;
+        }
     if (type == "d")
         // Création d'un tableau de valeur ordonné décroissant
-        for (int i = 0; i < taille; i++){
-            tab[i] = taille-i;}
-    
-    int heap[taille];
-    int i = 0;
-    while (i != taille){
-        tab
+        for (int i = 0; i < taille; i++)
+        {
+            tab[i] = taille-i;
+        }
+    for (int i = 0; i < (taille - 1); i++)
+    {
+        insert(tab,i);
     }
-
+    for (int i = 0; i < (taille - 1); i++)
+    {
+        remove(tab,(taille-1-i));
+    }
 }
 
 int main(){
@@ -65,7 +102,7 @@ int main(){
     //https://openclassrooms.com/forum/sujet/calcul-temps-execution-84649
     int time1 = 0;
     printf("Naif: \n");
-    tri_naif(type);
+    tri_tas(type);
     time1 = clock();
     printf("Temps d'execution = %d ms\n", time1);
     
